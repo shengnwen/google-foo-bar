a559c55688c9b2db93c06099fe584e4c64c36db9
shengnwen <shengwen@live.cn>
2015-11-27 12:26:32 -0500

    ch4-1 start

--- spy_snippets.py
+++ spy_snippets.py
@@ -5,21 +5,30 @@ __author__ = 'shengwen'
 # The search terms can appear in any order.
 
 document = "many google employees can program"
-searchTerms = ["google", "program"]
+searchTerms = ["google"]
 # output
 # "google employees can program"
 
 def answer(document, searchTerms):
     # your code here
     words = document.split(" ")
-    minLen = len(words)
-    i, j = 0, len(words) - 1
-    while i < j:
-        while words[i] not in searchTerms:
-            i += 1
-        while words[j] not in searchTerms:
-            j -= 1
-        
+    dict = {i:[] for i in searchTerms}
+    for (i,word) in list(enumerate(words)):
+        if word in searchTerms:
+            dict[word].append(i)
+    min_score = 2 * len(words)
+    result = None
+    for key in dict.keys():
+        for i in dict[key]:
+            indices = []
+            for other_key in dict.keys():
+                dists = [abs(i - x) for x in dict[other_key]]
+                indices.append(dict[other_key][dists.index(min(dists))])
+            score = max(indices) - min(indices) + 1
+            if score < min_score:
+                min_score = score
+                result = [min(indices), max(indices)]
+    return " ".join(words[result[0]:result[1] + 1])
 
-    return minLen
 
+print answer(document, searchTerms)
