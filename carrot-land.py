f27be92dec90863be3b530e7975708e2a2c8cb93
shengnwen <shengwen@live.cn>
2015-11-28 19:02:36 -0500

    ch4-1 finish

--- carrot-land.py
+++ carrot-land.py
@@ -25,19 +25,47 @@ def gcd(a, b):
         return b
     return gcd(b, a % b)
 
+def lattice_points(p1, p2):
+    return gcd(p1[0] - p2[0], p1[1] - p2[1]) - 1
+
+
+
 def answer(vertices):
     # your code here
     # use pick's algorithm: A = i + B/2 - 1
-    p0x, p0y = vertices[0]
-    p1x, p1y = vertices[1]
-    p2x, p2y = vertices[2]
-    _2Area = (-p1y*p2x + p0y*(-p1x + p2x) + p0x*(p1y - p2y) + p1x*p2y)
-    b1 = gcd(p0y - p1y, p0x - p1x)
-    b2 = gcd(p0y - p2y, p0x - p2x)
-    b3 = gcd(p1y - p2y, p1x - p2x)
-    B = b1 + b2 + b3
-    # calculate boundary points
-    return (_2Area + 2 - B)/2
+    xPoints = [i for i in sorted(vertices, key=lambda x: x[0])]
+    yPoints = [i for i in sorted(vertices, key=lambda x: x[1])]
+    j = xPoints[-1][0] - xPoints[0][0]
+    k = yPoints[-1][1] - yPoints[0][1]
+    points = (j - 1) * (k - 1)
+    # print "rec:" + str(points)
+    for i in range(3):
+        va = xPoints[i]
+        vb = xPoints[(i + 1)%3]
+        vc = xPoints[(i + 2)%3]
+
+        vba_x = abs(va[0] - vb[0])
+        vba_y = abs(va[1] - vb[1])
+
+        if vba_x != 0 and vba_y != 0:
+            points -= float((vba_x - 1) * (vba_y - 1) - lattice_points(va, vb)) / 2 + lattice_points(va, vb)
+
+        if i == 0 or i == 2 or va[1] == yPoints[0][1] or va[1] == yPoints[-1][1]:
+            continue
+        k = float(vb[1] - vc[1])/float(vb[0] - vc[0])
+        k1 = 0
+        k2 = 0
+        if va[1] > k * (va[0] - vb[0]) + vb[1]:
+            k1 = va[0] - xPoints[0][0]
+            k2 = yPoints[-1][1] - va[1]
+        else:
+            k1 = xPoints[-1][0] - va[0]
+            k2 = va[1] - yPoints[0][1]
+        points -= k1 * k2
+        # print "k1:" + str(k1) + ", k2:" + str(k2)
+    return int(points)
+
+
 
 
 print answer(vertices)
